995888c2dfe87d03ebf40c9b6d2b05a62d030e1e
ejschellekens <bramschellekens98@gmail.com>
2023-09-15 10:01:10 +0200

    Added c

--- Opdracht2.py
+++ Opdracht2.py
@@ -19,4 +19,6 @@ def create_mandel() -> np.array:
     #a[0] = 0
     #a[n] = a[n-1]**2 + complex(x,y)
     #if |a[n]| > 2 is n the diverging index. if n not between 0 en 100 -> 0
-    
+    c = complex([-1.5, 0.5], [-1, 1])
+
+create_mandel()
