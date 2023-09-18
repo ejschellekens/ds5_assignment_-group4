04b46830733a2e3aafe440b798ef3f7dc2cad24f
ejschellekens <bramschellekens98@gmail.com>
2023-09-18 12:55:29 +0200

    A berekent voor opdracht 2

--- Opdracht2.py
+++ Opdracht2.py
@@ -1,4 +1,6 @@
 import numpy as np
+from PIL import Image as im
+from matplotlib import pyplot as plt
 
 def draw_mandel(width : int):
     """Draws mandel
@@ -9,17 +11,33 @@ def draw_mandel(width : int):
     #x range [-1.5; 0.5]
     #y range [-1; 1]
     #diverging index (n) determs the pixel colour. Black if n = 0. Otherwise some white or blue
+    c = pixelToC(width)
+    a = create_mandel(c)
+    
+    
 
-def create_mandel() -> np.array:
+
+def create_mandel(c : np.array) -> np.array:
     """Creates the mandel points
     
     :return:
         numpy array: contains all points of the mandel
     """
     #a[0] = 0
-    #a[n] = a[n-1]**2 + complex(x,y)
+    #a[n] = a[n-1]**2 + c
     #if |a[n]| > 2 is n the diverging index. if n not between 0 en 100 -> 0
-    
+    a = [0]
+    index = 1
+    for i in c:
+        uitkomst = a[index-1]**2 + i
+        if abs(uitkomst) > 2:
+            a.append(uitkomst)
+        else:
+            a.append(0)
+
+    a = np.array(a)
+    return a
+
 def pixelToC(width : int) -> np.array:
     """Turns pixels into a complex number
     
@@ -37,4 +55,4 @@ def pixelToC(width : int) -> np.array:
 
     return c
 
-c = pixelToC(200)
+draw_mandel(200)
